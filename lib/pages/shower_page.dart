51dfdc1b27cac6318b66466b330b0e3a5e798852
Evgeny <afazhenya2001@gmail.com>
2022-11-03 19:18:46 +0300

    shower-slots

--- lib/pages/shower_page.dart
+++ lib/pages/shower_page.dart
@@ -1,43 +1,91 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:smart_dorm/blocs/generate_queue.dart';
+import 'package:smart_dorm/models/shower_timeslot.dart';
+import 'package:smart_dorm/models/user.dart';
+import 'package:smart_dorm/resources/repository.dart';
 
-class ShowerPage extends StatelessWidget {
+class ShowerPage extends StatefulWidget {
   const ShowerPage({super.key});
+  @override
+  State<ShowerPage> createState() => _ShowerPageState();
+}
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+class _ShowerPageState extends State<ShowerPage> {
+  final _repository = Repository();
+  late List<ShowerTimeSlot> timeslots = [];
+  late List<String> mappedNameData = [];
+  late final String format = 'HH:mm a';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchShowerTimeslots();
+  }
+
+  Future<void> fetchShowerTimeslots() async {
+    List<ShowerTimeSlot> timeslotsData =
+        await _repository.fetchShowerTimeSlot();
+
+    List<Future<User>> mappedNameDataFutures = [];
+    timeslotsData.forEach((timeSlot) {
+      mappedNameDataFutures.add(_repository.getUserById(timeSlot.userId));
+    });
+
+    var mappedNameDataResult = await Future.wait(mappedNameDataFutures);
+    print(mappedNameDataResult.map((user) => user.name));
+
+    setState(() {
+      timeslots = timeslotsData;
+      mappedNameData = mappedNameDataResult.map((user) => user.name).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shower timeslots"),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const AddShowerTimeslotPage(),
-                  ),
-                );
-              },
-              child: const Icon(
-                Icons.add,
+        appBar: AppBar(
+          title: const Text("Shower timeslots"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const AddShowerTimeslotPage(),
+                    ),
+                  );
+                },
+                child: const Icon(
+                  Icons.add,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          "This is shower page",
-          style: optionStyle,
+            )
+          ],
         ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: timeslots.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  child: ListTile(
+                      title: Text(mappedNameData.length > 0
+                          ? mappedNameData[index]
+                          : 'No data'),
+                      subtitle: Text(DateFormat(format)
+                          .format(timeslots[index].startTime.toDate())),
+                      leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "http://kazbas.ilovebasket.ru/team-logo/9723")),
+                      trailing: Icon(Icons.shower)));
+            }));
   }
 }
 
