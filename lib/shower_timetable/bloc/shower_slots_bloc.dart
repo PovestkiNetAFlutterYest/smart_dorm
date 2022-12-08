4e02ab2d23d87481a266b7ac664dda7f70003a0d
Evgeny A <sh.arslanov@tinkoff.ru>
2022-12-08 12:53:23 +0300

    part of shower inconsistency

--- lib/shower_timetable/bloc/shower_slots_bloc.dart
+++ lib/shower_timetable/bloc/shower_slots_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:smart_dorm/auth/dto/user.dart';
 import 'package:smart_dorm/shower_timetable/bloc/shower_slots_event.dart';
 import 'package:smart_dorm/shower_timetable/bloc/shower_slots_state.dart';
@@ -9,11 +10,27 @@ import '../resources/repository.dart';
 class ShowerSlotsBloc extends Bloc<ShowerSlotsEvent, ShowerSlotsState> {
   final ShowerSlotsRepository repository;
 
+  num toNumericValue(Timestamp timestamp){
+    DateTime dateTimeRowVal = DateTime.fromMicrosecondsSinceEpoch(timestamp.microsecondsSinceEpoch, isUtc: true);
+    return dateTimeRowVal.hour+60 + dateTimeRowVal.minute;
+  }
+
+  bool checkOnInconsistency(List<ShowerTimeSlot> timeslots){
+    for (var timeslot in timeslots) {
+      for (var timeSlotToCompare in timeslots){
+        print(toNumericValue(timeslot.startTime));
+        if (toNumericValue(timeslot.startTime) < toNumericValue(timeSlotToCompare.startTime) && toNumericValue(timeslot.startTime) < toNumericValue(timeSlotToCompare.startTime));
+      }
+    }
+    return true;
+  }
+
   ShowerSlotsBloc(this.repository) : super(ShowerSlotsEmptyState()) {
     on<UpdateShowerSlotsEvent>((event, emit) async {
       try {
         List<ShowerTimeSlot> timeSlotsData =
             await repository.fetchShowerTimeSlot();
+        print(checkOnInconsistency(timeSlotsData));
         List<Future<User?>> usersDataFutures = [];
         for (var timeSlot in timeSlotsData) {
           usersDataFutures.add(repository.getUserById(timeSlot.userId));
--- lib/shower_timetable/shower_page.dart
+++ lib/shower_timetable/shower_page.dart
@@ -8,31 +8,6 @@ import 'package:smart_dorm/shower_timetable/bloc/shower_slots_state.dart';
 import 'package:smart_dorm/shower_timetable/widgets/roommates_list.dart';
 import 'package:smart_dorm/shower_timetable/widgets/timeslots_appbar.dart';
 
-// class ShowerPage extends StatelessWidget {
-//   final SharedPreferences prefs;
-//   const ShowerPage({super.key, required this.prefs});
-//   @override
-//   Widget build(BuildContext context) {
-//
-//       return Scaffold(
-//             appBar: TimeslotsAppbar(prefs: prefs),
-//             body: BlocBuilder<ShowerSlotsBloc, ShowerSlotsState>(
-//               builder: (context, state) {
-//                 ShowerSlotsBloc bloc = context.read<ShowerSlotsBloc>();
-//
-//                 if (state is ShowerSlotsEmptyState) {
-//                   bloc.add(UpdateShowerSlotsEvent());
-//                   return const Center(child: CircularProgressIndicator());
-//                 }
-//                 if (state is ShowerSlotsSuccessState) {
-//                   return const RoommatesListWidget();
-//                 }
-//                 return const Text('Unhandled state');
-//               },
-//             ));
-//   }
-// }
-
 class ShowerPage extends StatefulWidget {
   const ShowerPage({ super.key, required this.prefs });
   final SharedPreferences prefs;
@@ -42,7 +17,6 @@ class ShowerPage extends StatefulWidget {
 }
 
 class _ShowerPageState extends State<ShowerPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
